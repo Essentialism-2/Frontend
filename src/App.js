e3c70f0dc0add7e46dbd6384aef1f98eb798ffab
Teague <teaguehannam@gmail.com>
2020-03-02 22:06:13 -0700

    Added Login and Registration components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/landingPage';
+
 import './App.css';
+import Login from './components/login';
+import Register from './components/register';
 
 function App() {
     return (
@@ -9,10 +12,13 @@ function App() {
             <div className='App'>
               <Switch>
                 <Route exact path='/' component={LandingPage} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
               </Switch>
             </div>
         </Router>
     );
 }
+
 
 export default App;
