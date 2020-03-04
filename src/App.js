ea4d746b01674daaa559d2442b61526586c87d55
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-03-04 13:15:38 -0800

    dashboard added along with add project button at bottom right

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ValuesForm from './components/ValuesForm';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
                 <Route exact path='/login' component={Login} />
                 <Route exact path='/register' component={Register} />
                 <Route exact path='/values-form' component={ValuesForm} />
+                <Route exact path='/dashboard' component={Dashboard} />
               </Switch>
             </div>
         </Router>
