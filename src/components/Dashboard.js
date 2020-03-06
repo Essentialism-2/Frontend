a6e6dfd16c526ea16f16a327d3d2e65c1158691f
ZaStack <stackzpersonal@gmail.com>
2020-03-06 07:30:32 -0800

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -36,62 +36,72 @@ import MoreIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
     grow: {
-        flexGrow: 1
-    },
-    menuButton: {
-        marginRight: theme.spacing(2)
-    },
-    title: {
+        flexGrow: 1,
+        
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+      title: {
         display: 'none',
         [theme.breakpoints.up('sm')]: {
-            display: 'block'
-        }
-    },
-    search: {
+          display: 'block',
+        },
+      },
+      search: {
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
         backgroundColor: fade(theme.palette.common.white, 0.15),
         '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25)
+          backgroundColor: fade(theme.palette.common.white, 0.25),
         },
         marginRight: theme.spacing(2),
         marginLeft: 0,
         width: '100%',
         [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto'
-        }
-    },
-    inputRoot: {
-        color: 'inherit'
-    },
-    inputInput: {
+          marginLeft: theme.spacing(3),
+          width: 'auto',
+        },
+      },
+      searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
         padding: theme.spacing(1, 1, 1, 7),
         transition: theme.transitions.create('width'),
         width: '100%',
         [theme.breakpoints.up('md')]: {
-            width: 200
-        }
-    },
-    sectionDesktop: {
+          width: 200,
+        },
+      },
+      sectionDesktop: {
         display: 'none',
         [theme.breakpoints.up('md')]: {
-            display: 'flex'
-        }
-    },
-    sectionMobile: {
+          display: 'flex',
+        },
+      },
+      sectionMobile: {
         display: 'flex',
         [theme.breakpoints.up('md')]: {
-            display: 'none'
-        }
-    },
+          display: 'none',
+        },
+      },
     root: {
-        '& > *': {
-            margin: theme.spacing(1)
-        }
+      '& > *': {
+        margin: theme.spacing(1),
+      },
     },
     extendedIcon: {
-        marginRight: theme.spacing(1)
+      marginRight: theme.spacing(1),
     },
     addProject: {
         position: 'absolute',
@@ -104,6 +114,7 @@ const useStyles = makeStyles(theme => ({
 
     },
     appBar: {
+<<<<<<< HEAD
 
       backgroundColor: theme.pallette.primary.main
     },
@@ -112,39 +123,50 @@ const useStyles = makeStyles(theme => ({
         bottom: 10,
         left: 10
 
+=======
+      backgroundColor: theme.pallette.primary.main
+>>>>>>> parent of bddbf97... Merge branch 'clean-up' of https://github.com/Essentialism-2/Frontend into clean-up
     }
-}));
+  }));
 
 const Dashboard = () => {
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+<<<<<<< HEAD
     const [editing, setEditing] = useState(false);
 
+=======
+  
+>>>>>>> parent of bddbf97... Merge branch 'clean-up' of https://github.com/Essentialism-2/Frontend into clean-up
     const isMenuOpen = Boolean(anchorEl);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
+  
     const handleProfileMenuOpen = event => {
-        setAnchorEl(event.currentTarget);
+      setAnchorEl(event.currentTarget);
     };
-
+  
     const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
+      setMobileMoreAnchorEl(null);
     };
+<<<<<<< HEAD
 
     const handleEditing = () => {
       setEditing(!editing)
   }
 
+=======
+  
+>>>>>>> parent of bddbf97... Merge branch 'clean-up' of https://github.com/Essentialism-2/Frontend into clean-up
     const handleMenuClose = () => {
-        setAnchorEl(null);
-        handleMobileMenuClose();
+      setAnchorEl(null);
+      handleMobileMenuClose();
     };
-
+  
     const handleMobileMenuOpen = event => {
-        setMobileMoreAnchorEl(event.currentTarget);
+      setMobileMoreAnchorEl(event.currentTarget);
     };
-
+  
     const menuId = 'primary-search-account-menu';
     const renderMenu = (
       <Menu
@@ -158,51 +180,57 @@ const Dashboard = () => {
       >
         <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
         <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+<<<<<<< HEAD
         <SignOut />
       </Menu>
 
+=======
+      </Menu>
+>>>>>>> parent of bddbf97... Merge branch 'clean-up' of https://github.com/Essentialism-2/Frontend into clean-up
     );
-
+  
     const mobileMenuId = 'primary-search-account-menu-mobile';
     const renderMobileMenu = (
-        <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}>
-            <MenuItem>
-                <IconButton aria-label='show 4 new mails' color='inherit'>
-                    <Badge badgeContent={4} color='secondary'>
-                        <MailIcon />
-                    </Badge>
-                </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton
-                    aria-label='show 11 new notifications'
-                    color='inherit'>
-                    <Badge badgeContent={11} color='secondary'>
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
-            </MenuItem>
-            <MenuItem onClick={handleProfileMenuOpen}>
-                <IconButton
-                    aria-label='account of current user'
-                    aria-controls='primary-search-account-menu'
-                    aria-haspopup='true'
-                    color='inherit'>
-                    <AccountCircle />
-                </IconButton>
-                <p>Profile</p>
-            </MenuItem>
-        </Menu>
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileMenuClose}
+      >
+        <MenuItem>
+          <IconButton aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <p>Messages</p>
+        </MenuItem>
+        <MenuItem>
+          <IconButton aria-label="show 11 new notifications" color="inherit">
+            <Badge badgeContent={11} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <p>Notifications</p>
+        </MenuItem>
+        <MenuItem onClick={handleProfileMenuOpen}>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <p>Profile</p>
+        </MenuItem>
+      </Menu>
     );
+
+
 
     return (
         <div className={classes.grow}>
@@ -268,6 +296,7 @@ const Dashboard = () => {
             </div>
           </Toolbar>
         </AppBar>
+<<<<<<< HEAD
         <TopThree handleEditing={handleEditing} editing={editing} />
         <ProjectsForm handleEditing={handleEditing} editing={editing} />
         {renderMobileMenu}
@@ -276,10 +305,20 @@ const Dashboard = () => {
         <Fab onClick={handleEditing} className={classes.bottomLeftFixed}  color="primary" aria-label="add">
             <SettingsIcon />
         </Fab>
+=======
+        <TopThree />
+        <ProjectsForm />
+        {renderMobileMenu}
+        {renderMenu}
+
+>>>>>>> parent of bddbf97... Merge branch 'clean-up' of https://github.com/Essentialism-2/Frontend into clean-up
       </div>
 
     )
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of bddbf97... Merge branch 'clean-up' of https://github.com/Essentialism-2/Frontend into clean-up
 
 export default Dashboard;
